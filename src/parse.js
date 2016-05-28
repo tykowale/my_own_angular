63c0667624830e52f23ad49d2ad59c361303ac99
tykowale <tykowale@gmail.com>
2016-05-28 16:37:01 -0500

    high level structure of parser

---
+++ src/parse.js
@@ -0,0 +1,42 @@
+'use strict';
+
+module.exports = parse;
+
+function parse(expr) {
+    var lexer = new Lexer();
+    var parser = new Parser(lexer);
+    return parser.parse(expr);
+}
+
+function Lexer() {}
+
+Lexer.prototype.lex = function(text) {
+    text;
+};
+
+function AST(lexer) {
+    this.lexer = lexer;
+}
+
+AST.prototype.ast = function(text) {
+    this.tokens = this.lexer.lex(text);
+};
+
+function ASTCompiler(astBuilder) {
+    this.astBuilder = astBuilder;
+}
+
+ASTCompiler.prototype.compile = function(text) {
+    var ast = this.astBuilder.ast(text);
+    ast;
+};
+
+function Parser(lexer) {
+    this.lexer = lexer;
+    this.ast = new AST(this.lexer);
+    this.astCompiler = new ASTCompiler(this.ast);
+}
+
+Parser.prototype.parse = function(text) {
+    return this.astCompiler.compile(text);
+};
