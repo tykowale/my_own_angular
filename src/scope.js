3e841a6a8cbf692fbb92c893094d3f3d783a42fb
tykowale <tykowale@gmail.com>
2016-05-27 21:46:20 -0500

    propagate down on broadcast

--- src/scope.js
+++ src/scope.js
@@ -430,7 +430,11 @@ Scope.prototype.$broadcast = function(eventName) {
     };
     var listenerArgs = [event].concat(_.tail(arguments));
 
-    this.$$fireEventOnScope(eventName, listenerArgs);
+    this.$$everyScope(function(scope) {
+        scope.$$fireEventOnScope(eventName, listenerArgs);
+        return true;
+    });
+
     return event;
 };
 
