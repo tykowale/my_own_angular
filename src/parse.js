01e3015f781c5f250b42af5ae51675672fb24895
tykowale <tykowale@gmail.com>
2016-05-29 13:27:43 -0500

    parse scientific notation

--- src/parse.js
+++ src/parse.js
@@ -36,11 +36,24 @@ Lexer.prototype.isNumber = function(ch) {
 Lexer.prototype.readNumber = function() {
     var number = '';
     while (this.index < this.text.length) {
-        var ch = this.text.charAt(this.index);
+        var ch = this.text.charAt(this.index).toLowerCase();
         if (ch === '.' || this.isNumber(ch)) {
             number += ch;
         } else {
-            break;
+            var nextCh = this.peek();
+            var prevCh = number.charAt(number.length - 1);
+
+            if (ch === 'e' && this.isExpOperator(nextCh)) {
+                number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' &&
+                nextCh && this.isNumber(nextCh)) {
+                number += ch;
+            } else if (this.isExpOperator(ch) && prevCh === 'e' &&
+                (!nextCh || !this.isNumber(nextCh))) {
+                throw 'Invalid Exponent';
+            } else {
+                break;
+            }
         }
         this.index++;
     }
@@ -55,6 +68,10 @@ Lexer.prototype.peek = function() {
     return this.index < this.text.length - 1 ?
         this.text.charAt(this.index + 1) :
         false;
+};
+
+Lexer.prototype.isExpOperator = function(ch) {
+    return ch === '-' || ch === '+' || this.isNumber(ch);
 };
 
 function AST(lexer) {
