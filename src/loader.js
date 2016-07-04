5d1ae77641d7e884db7d8ce4faca22fb3fa2f8c0
tykowale <tykowale@gmail.com>
2016-07-04 17:41:18 -0500

    start modules and loader

---
+++ src/loader.js
@@ -0,0 +1,26 @@
+'use strict';
+
+function setupModuleLoader(window) {
+    var ensure = function(obj, name, factory) {
+        return obj[name] || (obj[name] = factory());
+    };
+
+    var angular = ensure(window, 'angular', Object);
+
+    var createModule = function(name, requires) {
+        var moduleInstance = {
+            name: name,
+            requires: requires
+        };
+
+        return moduleInstance;
+    };
+
+    ensure(angular, 'module', function() {
+        return function(name, requires) {
+            return createModule(name, requires);
+        };
+    });
+}
+
+module.exports = setupModuleLoader;
