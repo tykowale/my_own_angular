356a3375b452c5101f12096268117102c462adf4
tykowale <tykowale@gmail.com>
2016-07-05 21:17:44 -0500

    very basic injector

--- test/injector.spec.js
+++ test/injector.spec.js
@@ -4,13 +4,29 @@ var setupModuleLoader = require('../src/loader');
 var createInjector = require('../src/injector');
 
 describe('injector', function() {
+    var angular;
+
     beforeEach(function() {
         delete window.angular;
         setupModuleLoader(window);
+        angular = window.angular;
     });
 
     it('can be created', function() {
         var injector = createInjector([]);
         expect(injector).toBeDefined();
+    });
+
+    it('has a constant that has been registered to a module', function() {
+        var module = angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(true);
+    });
+
+    it('does not have a non-registered constant', function() {
+        var module = angular.module('myModule', []);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(false);
     });
 });
