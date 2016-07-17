9ab275829556a4d3177d402caf63c75e4120cb1f
tykowale <tykowale@gmail.com>
2016-07-17 15:27:41 -0500

    execute run blocks after module loading

--- src/injector.js
+++ src/injector.js
@@ -129,6 +129,8 @@ function createInjector(modulesToLoad, strictDi) {
         });
     }
 
+    var runBlocks = [];
+
     _.forEach(modulesToLoad, function loadModule(moduleName) {
         if (!loadedModules.hasOwnProperty(moduleName)) {
             loadedModules[moduleName] = true;
@@ -136,7 +138,12 @@ function createInjector(modulesToLoad, strictDi) {
             _.forEach(module.requires, loadModule);
             runInvokeQueue(module._invokeQueue);
             runInvokeQueue(module._configBlocks);
+            runBlocks = runBlocks.concat(module._runBlocks);
         }
+    });
+
+    _.forEach(runBlocks, function(runBlock) {
+        instanceInjector.invoke(runBlock);
     });
 
     return instanceInjector;
