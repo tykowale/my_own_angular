73790eb9627de52ec66173b082c7ac6b4582cc2a
tykowale <tykowale@gmail.com>
2016-07-23 12:54:27 -0500

    register values

--- src/injector.js
+++ src/injector.js
@@ -46,10 +46,13 @@ function createInjector(modulesToLoad, strictDi) {
             }
             providerCache[key + 'Provider'] = provider;
         },
-        factory: function(key, factoryFn) {
+        factory: function(key, factoryFn, enforce) {
             this.provider(key, {
-                $get: enforceReturnValue(factoryFn)
+                $get: !enforce ? factoryFn : enforceReturnValue(factoryFn)
             });
+        },
+        value: function(key, value) {
+            this.factory(key, _.constant(value), false);
         }
     };
 
