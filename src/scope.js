56c5e694af7a9d0ed5e9f0cdc2b99d1804195b80
tykowale <tykowale@gmail.com>
2016-07-24 18:50:08 -0500

    configure TTL

--- src/scope.js
+++ src/scope.js
@@ -3,6 +3,15 @@
 var _ = require('lodash');
 
 function $RootScopeProvider() {
+    var TTL = 10;
+
+    this.digestTtl = function(value) {
+        if(_.isNumber(value)) {
+            TTL = value;
+        }
+
+        return TTL;
+    };
 
     this.$get = ['$parse',
         function($parse) {
@@ -100,7 +109,7 @@ function $RootScopeProvider() {
             };
 
             Scope.prototype.$digest = function() {
-                var ttl = 10;
+                var ttl = TTL;
                 var dirty;
 
                 this.$root.$$lastDirtyWatch = null;
@@ -123,7 +132,7 @@ function $RootScopeProvider() {
                     dirty = this.$$digestOnce();
                     if ((dirty || this.$$asyncQueue.length) && !(ttl--)) {
                         this.$clearPhase();
-                        throw '10 digest iterations reached';
+                            throw TTL + ' digest iterations reached';
                     }
                 } while (dirty || this.$$asyncQueue.length);
                 this.$clearPhase();
